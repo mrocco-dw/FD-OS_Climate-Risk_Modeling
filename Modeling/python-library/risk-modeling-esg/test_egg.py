efac231ebe4f7ad7e31dfec60354ce79b4b04dcb
Joao Pedro Afonso Cerqueira <114918447+jpacerqueira@users.noreply.github.com>
2023-09-18 17:20:06 +0100

    Create test_egg.py
    create egg for python module function

---
+++ Modeling/python-library/risk-modeling-esg/test_egg.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('risk-modeling-esg/output/esg_risk_training-0.0.1-py3.9.egg')
+
+import esg_risk_training
+
+
+obj = esg_risk_training.EsgRiskTrainingClass()
+obj.set_x(29)
+print(obj.get_x())
