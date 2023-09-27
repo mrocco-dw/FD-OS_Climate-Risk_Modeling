a7221fe344fae4ce577ce423df2d207d64b9de1b
jcerqueira <jpacerqueira@gmail.com>
2023-09-27 11:28:52 +0100

    GitHub Actions with smoke tests

--- Modeling/python-library/risk-modeling-osf/conftest.py
+++ Modeling/python-library/risk-modeling-osf/conftest.py
@@ -4,7 +4,10 @@
 import pytest
 #
 def pytest_sessionstart(session=0):
-    pass
+    with pytest.raises(ValueError) as execinfo: 
+        execinfo.value = 'TEST APP'
+    assert str(execinfo.value) == 'TEST APP'
+#    pass
 #    return 1
 #
 # pytest setup stuff
