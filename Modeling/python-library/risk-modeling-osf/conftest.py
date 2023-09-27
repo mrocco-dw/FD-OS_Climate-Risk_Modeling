999a7e470999de5f743f1cff692529887b9fc281
jcerqueira <jpacerqueira@gmail.com>
2023-09-27 12:21:09 +0100

    GitHub Actions smoke test with custom var names

--- Modeling/python-library/risk-modeling-osf/conftest.py
+++ Modeling/python-library/risk-modeling-osf/conftest.py
@@ -2,16 +2,21 @@
 # conftest.py
 #
 import pytest
+import osf_risk_training
 #
 def pytest_sessionstart(session=0, execinfovalue='TEST APP'):
-    #with pytest.raises(ValueError) as execinfo: 
-    #    execinfo.value = 'TEST APP'
-    assert str(execinfovalue) == 'TEST APP'
+    with pytest.raises(ValueError) as execinfo: 
+        obj = osf_risk_training.OsfRiskTrainingClass()
+        val = []
+        dummy = []
+        dummy.val = 20
+        val.obj = obj.set_x(19)
+    assert str(val.obj) == dummy.val
 #    pass
 #    return 1
 #
 # pytest setup stuff
-# pytest.main()
+#pytest.main()
 #
 pytest_sessionstart(session=0, execinfovalue='TEST APP')
 #
