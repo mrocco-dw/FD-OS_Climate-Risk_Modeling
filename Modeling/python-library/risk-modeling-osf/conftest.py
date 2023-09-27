f55658ba3e1eab0ff0946b4dd98f68a0922d851f
jcerqueira <jpacerqueira@gmail.com>
2023-09-27 12:00:44 +0100

    smoke test with custom var names

--- Modeling/python-library/risk-modeling-osf/conftest.py
+++ Modeling/python-library/risk-modeling-osf/conftest.py
@@ -4,7 +4,7 @@
 import pytest
 #
 def pytest_sessionstart(session=0):
-    execinfovalue
+    execinfovalue = ""
     with pytest.raises(ValueError) as execinfo: 
         execinfovalue = 'TEST APP'
     assert str(execinfovalue) == 'TEST APP'
