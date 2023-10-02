6b1cf938741b922d0f73f2dbd5c7c8aa482413b1
jcerqueira <jpacerqueira@gmail.com>
2023-10-02 14:53:43 +0100

    indepotency on launch scripts for Linux wsl or Darwin MacOS launch

--- launch-webapp.sh
+++ launch-webapp.sh
@@ -2,11 +2,11 @@
 #
 # python3 is required as main python
 #
-PATTERN_X="^(MINGW64_NT-10.0-19044$|MINGW64_NT-10.*-*$|Unix*$)"
-if [[ $(uname) =~ $PATTERN_X ]]; then
-    export PYTHON_BUILD_COMMAND=python
+PATTERN_X="+(Linux*|wsl*)"
+if [[ "$(uname)" == $PATTERN_X ]]; then
+    export PYTHON_BUILD_COMMAND=python.exe # FOR WSL / Linux WSL
 else
-    export PYTHON_BUILD_COMMAND=python.exe
+    export PYTHON_BUILD_COMMAND=python # For MacBook / IOs
 fi
 # idenpotent local build egg and launch webapp server
 rm -rf Modeling/python-library/webapp/output/osf_model_app-0.0.1-py3.11.egg
